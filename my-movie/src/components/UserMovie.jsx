ac41f5f30e1fc523279715d6c77aaa1a46c0d445
Behruz Jumanov <behruzjumano55@gmail.com>
2026-04-29 11:20:08 +0500

    first commit

---
+++ my-movie/src/components/UserMovie.jsx
@@ -0,0 +1,23 @@
+// export default function MovieType({ movies }) {
+//   console.log(movies, "Salom");
+
+//   return (
+//     <div className="append">
+//       {movies?.map((movie) => {
+//         return (
+//           <div className="movie" key={movie.id}>
+//             <img
+//               src={"https://image.tmdb.org/t/p/w500/" + movie.backdrop_path}
+//               alt={movie.title}
+//             />
+//             <div className="movie-info">
+//               <h3>{movie.title}</h3>
+//               <span className="orange">{movie.vote_average}</span>
+//             </div>
+//             <span className="date">{movie.release_date}</span>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
